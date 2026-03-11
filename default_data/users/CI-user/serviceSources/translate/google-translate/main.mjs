172aa95b0950f8d36ed9af103ff6db849e4524e5
steve02081504 <steve02081504@EDEN.fukwold>
2026-03-12 00:53:13 +0800

    update codebase

--- default_data/users/CI-user/serviceSources/translate/google-translate/main.mjs
+++ default_data/users/CI-user/serviceSources/translate/google-translate/main.mjs
@@ -1,5 +1,6 @@
 import path from 'node:path'
 
+import { setPartData } from '../../../../../../src/public/parts/shells/config/src/manager.mjs'
 import { loadJsonFile, saveJsonFile } from '../../../../../../src/scripts/json_loader.mjs'
 import { loadPart } from '../../../../../../src/server/parts_loader.mjs'
 
@@ -20,8 +21,13 @@ const defaultInterfaces = {
 		 * @returns {Promise<void>}
 		 */
 		async SetData(new_data) {
-			if (new_data.generator) data.generator = new_data.generator
-			if (new_data.config) data.config = new_data.config
+			if (new_data !== data) {
+				if (new_data.generator) data.generator = new_data.generator
+				if (new_data.config) { // 保持config对象不变，确保saveConfig有效
+					for (const key in data.config ??= {}) delete data.config[key]
+					Object.assign(data.config, new_data.config)
+				}
+			}
 			saveJsonFile(configPath, data)
 		},
 		/**
@@ -34,11 +40,13 @@ const defaultInterfaces = {
 	}
 }
 
+const my_name = path.basename(import.meta.dirname)
+
 /**
  * 翻译服务源模块。
  */
 export default {
-	filename: path.basename(import.meta.dirname),
+	filename: my_name,
 	/**
 	 * 加载翻译服务源。
 	 * @param {object} root0 - 参数对象。
@@ -49,10 +57,10 @@ export default {
 		const manager = await loadPart(username, 'serviceSources/translate')
 		Object.assign(this, await manager.interfaces.serviceSourceType.loadFromConfigData(username, data, {
 			/**
-			 * 保存配置。
-			 * @param {any} newdata - 新的配置数据。
+			 * 将当前配置保存到部件数据。
+			 * @returns {void}
 			 */
-			SaveConfig: (newdata) => { saveJsonFile(configPath, Object.assign(data, newdata)) }
+			SaveConfig: () => setPartData(username, `serviceSources/translate/${my_name}`, data)
 		}))
 		Object.assign(this.interfaces, defaultInterfaces)
 	},
